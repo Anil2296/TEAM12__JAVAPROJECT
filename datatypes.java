cff239c041532c9c5f789acf4a4ca3d88ed065ed
Anil2296 <anilkumarnagina007@gmail.com>
2022-07-20 20:41:47 +0530

    Matrix Subtraction

---
+++ datatypes.java
@@ -0,0 +1,22 @@
+class DataType
+{
+public static void main(String args[])
+{
+int x=10;
+float y=10;
+short z=10;
+double a=10;
+boolean b=fsalse;
+char c=10;
+byte d=10;
+long e=10;
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+}}
+
